5e6d4cac4c8c4d3895a7e10f43c152d53972d7aa
misoju <trantrung0195@gmail.com>
2020-04-08 22:39:06 +0700

    thuc hanh phan mang

---
+++ Module2/src/B1_NgonNguJava/BaiTap/DocSoThanhChu.java
@@ -0,0 +1,49 @@
+package B1_NgonNguJava.BaiTap;
+
+import java.util.Scanner;
+
+public class DocSoThanhChu {
+    public static void main(String[] args){
+        int readNumber;
+        Scanner scnumber = new Scanner(System.in);
+        System.out.print("Enter your number: ");
+        readNumber = scnumber.nextInt();
+        if (readNumber <= 10) {
+            switch (readNumber) {
+                case 1:
+                    System.out.println("one");
+                    break;
+                case 2:
+                    System.out.println("two");
+                    break;
+                case 3:
+                    System.out.println("three");
+                    break;
+                case 4:
+                    System.out.println("four");
+                    break;
+                case 5:
+                    System.out.println("five");
+                    break;
+                case 6:
+                    System.out.println("six");
+                    break;
+                case 7:
+                    System.out.println("seven");
+                    break;
+                case 8:
+                    System.out.println("eight");
+                    break;
+                case 9:
+                    System.out.println("nine");
+                    break;
+                case 10:
+                    System.out.println("ten");
+                    break;
+            }
+        }
+        else{
+            System.out.println("number over 10");
+        }
+    }
+}
