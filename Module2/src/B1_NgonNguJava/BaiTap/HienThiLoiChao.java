dd60d263f31d7323a32368130eb69e4255fa1d55
misoju <trantrung0195@gmail.com>
2020-04-08 09:21:57 +0700

    first commit

---
+++ Module2/src/B1_NgonNguJava/BaiTap/HienThiLoiChao.java
@@ -0,0 +1,15 @@
+package B1_NgonNguJava.BaiTap;
+
+import java.util.Scanner;
+
+public class HienThiLoiChao {
+    public static void main(String[] args){
+        //nhap ten
+        Scanner scname = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name1 = scname.nextLine();
+        //in ten
+        System.out.println("Hello: " + name1);
+    }
+}
+
---
+++ Module2/src/B1_NgonNguJava/ThucHanh/timUocSoChung.java
@@ -0,0 +1,25 @@
+package NgonNguLapTrinhJava.B1_JavaOverView.ThucHanh;
+
+import java.util.Scanner;
+
+public class timUocSoChung {
+    public static void main(String[] args){
+        int a,b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        a = sc.nextInt();
+        System.out.println("Enter b: ");
+        b = sc.nextInt();
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (a == 0 || b == 0)
+            System.out.println("No greatest common factor");
+        while (a != b) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+        System.out.println("Greatest common factor: " + a);
+    }
+}
